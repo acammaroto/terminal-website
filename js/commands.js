e3c8928b5b805c862ec66f800bebc62b40e4cd95
Angelo Cammaroto <hi@angelocammaroto.com>
2023-02-07 14:43:28 +0100

    first commit

---
+++ js/commands.js
@@ -0,0 +1,88 @@
+var youtube = "https://www.youtube.com/AngeloCammaroto";
+var password = "acammaroto";
+var linkedin = "https://www.linkedin.com/in/angelo-cammaroto/";
+var instagram = "https://www.instagram.com/angelo.cammaroto/";
+var github = "https://github.com/acammaroto/";
+var email = 'mailto:hi@angelocammaroto.com';
+
+whois = [
+  "<br>",
+  "Hey, I'm Angelo!👋",
+  "I'm a Software Engineer and content creator, who builds engaging websites (or mobile apps) like this one",
+  "and makes Instagram videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for big tech companies in Italy.",
+  "While doing all of that, I documented my coding journey on Instagram - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software", "development & computer science.",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  //'youtube        <a href="' + youtube + '" target="_blank">https://www.youtube.com/AngeloCammaroto' + "</a>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin/angelo-cammaroto' + "</a>",
+  'instagram      <a href="' + instagram + '" target="_blank">instagram/angelo.cammaroto' + '</a>',
+  'github         <a href="' + github + '" target="_blank">github/acammaroto' + "</a>",
+  "<br>"
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>"
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>"
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Angelo?',
+  '<span class="command">whoami</span>         Who are you?',
+  //'<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">Angelo Cammaroto (AC) Not A Corporation. All knights reserved.</span>',
+"              ________",
+"         _,.-Y  |  |  Y-._",
+"      .-~\"   ||  |  |  |   \"-.",
+"     I\" \"\"==\"|\" !\"\"! \"|\"[]\"\"|     _____",
+"    L__  [] |..------|:   _[----I\" .-{\"-.",
+"    I___|  ..| l______|l_ [__L]_[I_/r(=}=-P",
+"   [L______L_[________]______j~  '-=c_]/=-^",
+"    \\_I_j.--.\\==I|I==_/.--L_]",
+"      [_((==)[`-----\\\"](==)j",
+"         I--I\"~~\"\"\"~~\"I--I",
+"         |[]|         |[]|",
+"         l__j         l__j",
+"         |!!|         |!!|",
+"         |..|         |..|",
+"         ([])         ([])        _                 _        ___                               _       ",
+"         ]--[         ]--[       /_\\  _ _  __ _ ___| |___   / __|__ _ _ __  _ __  __ _ _ _ ___| |_ ___ ",
+"         [_L]         [_L]      / _ \\| ' \\/ _` / -_| / _ \\ | (__/ _` | '  \\| '  \\/ _` | '_/ _ |  _/ _ \\" , 
+"        /|..|\\       /|..|\\    /_/ \\_|_||_\\__, \\___|_\\___/  \\___\\__,_|_|_|_|_|_|_\\__,_|_| \\___/\\__\\___/",
+"       `=}--{='     `=}--{='              |___/                                                        ",
+"      .-^--r-^-.   .-^--r-^-.",
+"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~",
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
